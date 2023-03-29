d6355c4b3ed4b076a845adc42b038a09af726695
Ivan Ryzhov <xXlAlflXx@gmail.com>
2023-03-30 02:37:33 +0300

    saaSXsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -42,7 +42,7 @@ class App extends Component {
     this.setState({
       search: true,
       rated: false,
-      films: null,
+      films: [],
     })
   }
   onRated = (id) => {
--- src/components/Movie-filter/Movie-filter.js
+++ src/components/Movie-filter/Movie-filter.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const MovieFilter = ({ onRated, id, onSearch }) => {
   return (
     <div className="movie-filter">
-      <button className="movie-filter__button-search movie-filter__buttons" onClick={() => onSearch}>
+      <button className="movie-filter__button-search movie-filter__buttons" onClick={onSearch}>
         Search
       </button>
       <button className="movie-filter__button-rated movie-filter__buttons" onClick={() => onRated(id)}>
